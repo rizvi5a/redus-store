2b4b1e1edc8f1beff445703354a662c419748a94
syed Rizvi <rizvi5a@yahoo.com>
2021-08-04 21:37:36 -0500

    seeded data in mongoatlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Syed123:Syed123@cluster0.bvwpn.mongodb.net/mernshopping?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
